11050839e62424c9f55e8315df945f569c0ff42b
Denis Stanovoy <stanovoyd@yandex.ru>
2022-10-28 17:49:23 +0300

    logic overhaul

--- tetris.js
+++ tetris.js
@@ -1,10 +1,10 @@
-const $contentWrapper = document.getElementById("tetris__content-wrapper");
-const $playBtn = document.getElementById("tetris__play-btn");
-const $nextFigure = document.getElementById("tetris__next-figure");
-const $score = document.getElementById("tetris__score");
-const $record = document.getElementById("tetris__record");
-const $level = document.getElementById("tetris__level");
-let $initialGameSpeed = document.getElementById("tetris__speed");
+const $contentWrapper = document.getElementById("content-wrapper");
+const $playBtn = document.getElementById("play-btn");
+const $nextFigure = document.getElementById("next-figure");
+const $scoreValue = document.getElementById("score");
+const $record = document.getElementById("record");
+const $level = document.getElementById("level");
+const $initialGameSpeedInput = document.getElementById("speed");
 
 const $playingField = document.createElement("div");
 $playingField.classList.add("tetris__playing-field");
@@ -16,29 +16,127 @@ let levelInnerHtml = 1;
 
 const FIELD_WIDTH = 10;
 const FIELD_HEIGHT = 20;
+const FIELD_SIZE = FIELD_WIDTH * FIELD_HEIGHT;
+
+const PREVIEW_BOARD_WIDTH = 4;
+const PREVIEW_BOARD_HEIGHT = 4;
+const PREVIEW_BOARD_SIZE = PREVIEW_BOARD_WIDTH * PREVIEW_BOARD_HEIGHT;
 
 let playBoard = new Array(FIELD_HEIGHT)
   .fill(null)
   .map(() => new Array(FIELD_WIDTH).fill(0));
 
-console.log(playBoard);
+let previewBoard = new Array(PREVIEW_BOARD_HEIGHT)
+  .fill(null)
+  .map(() => new Array(PREVIEW_BOARD_WIDTH).fill(0));
 
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < FIELD_SIZE; i++) {
   const $divCell = document.createElement("div");
   $divCell.classList.add("tetris__cell");
   $playingField.append($divCell);
 }
 
+for (let i = 0; i < PREVIEW_BOARD_SIZE; i++) {
+  const $divCell = document.createElement("div");
+  $divCell.classList.add("tetris__cell");
+  $nextFigure.append($divCell);
+}
+
+const figures = [
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+];
+
+const figuresNameList = Object.keys(figures);
+
+const colors = {
+  1: "blue",
+  2: "goldenrod",
+  3: "yellow",
+  4: "purple",
+  5: "red",
+  6: "violet",
+  7: "green",
+};
+const colorsNameList = Object.keys(colors);
+
+const getRandomNum = (max) => {
+  return Math.ceil(Math.random() * max);
+};
+const colorIndex = colorsNameList.length - 1;
+
+const randomColor = getRandomNum(colorIndex);
+
+const getNewFigure = () => {
+  const randomColor = colors[getRandomNum(colorIndex)];
+
+  const randomNumber = getRandomNum(colorIndex);
+  const midfield = figuresNameList.length - 1;
+  const figure = figures[figuresNameList[getRandomNum(midfield)]];
+  const randomNumbsInFigure = figure.map((item) => {
+    return item.map((elem) => {
+      if (elem === 1) {
+        elem = randomNumber;
+      }
+      return elem;
+    });
+  });
+  console.log(randomNumbsInFigure);
+  return {
+    x: Math.ceil((FIELD_WIDTH - 2) / 2),
+    y: 0,
+    shape: randomNumbsInFigure,
+    color: randomColor,
+  };
+};
+
 let colorValue;
+
+let figure = getNewFigure();
 
 const update = () => {
   $playingField.innerHTML = "";
   colorValue = figure.color;
   for (let y = 0; y < playBoard.length; y++) {
     for (let x = 0; x < playBoard[y].length; x++) {
-      if (playBoard[y][x] === 1 || playBoard[y][x] === 9) {
+      if (playBoard[y][x] !== 0) {
         $divTetris = document.createElement("div");
-        $divTetris.classList.add(`${colorValue}`);
+        $divTetris.classList.add("tetris__cell");
+        $divTetris.style.backgroundColor = `${colorValue}`;
+        $divTetris.setAttribute("id", colorValue);
         $playingField.append($divTetris);
       } else {
         $divTetris = document.createElement("div");
@@ -50,95 +148,28 @@ const update = () => {
   if (hasCollision()) {
     return;
   }
-  let divTetrisPreview = "";
+  $nextFigure.innerHTML = "";
   for (let y = 0; y < nextFigureElem.shape.length; y++) {
     for (let x = 0; x < nextFigureElem.shape[y].length; x++) {
       if (nextFigureElem.shape[y][x]) {
-        divTetrisPreview += `<div class="${colorValue}"></div>`;
+        $divTetris = document.createElement("div");
+        $divTetris.classList.add("tetris__cell");
+        $divTetris.style.backgroundColor = `${colorValue}`;
+        $nextFigure.append($divTetris);
       } else {
-        divTetrisPreview += '<div class="tetris__cell"></div>';
+        $divTetris = document.createElement("div");
+        $divTetris.classList.add("tetris__cell");
+        $nextFigure.append($divTetris);
       }
     }
-    divTetrisPreview += "<br/>";
   }
-  $nextFigure.innerHTML = divTetrisPreview;
 };
-
-const figures = {
-  J: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-  ],
-  I: [
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-  ],
-  O: [
-    [1, 1],
-    [1, 1],
-  ],
-  L: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1],
-  ],
-  Z: [
-    [1, 1, 0],
-    [0, 1, 1],
-    [0, 0, 0],
-  ],
-  T: [
-    [1, 1, 1],
-    [0, 1, 0],
-    [0, 0, 0],
-  ],
-  S: [
-    [0, 1, 1],
-    [1, 1, 0],
-    [0, 0, 0],
-  ],
-};
-const figuresNameList = Object.keys(figures);
-
-const colors = {
-  1: "tetris__blue",
-  2: "tetris__goldenrod",
-  3: "tetris__yellow",
-  4: "tetris__purple",
-  5: "tetris__red",
-  6: "tetris__violet",
-  7: "tetris__green",
-};
-const colorsNameList = Object.keys(colors);
-
-const getRandomNum = (max) => {
-  return Math.ceil(Math.random() * max);
-};
-const colorIndex = colorsNameList.length - 1;
-const randomColor = colors[getRandomNum(colorIndex)];
-
-const getNewFigure = () => {
-  const randomColor = colors[getRandomNum(colorIndex)];
-  const midfield = figuresNameList.length - 1;
-  const figure = figures[figuresNameList[getRandomNum(midfield)]];
-  return {
-    x: Math.ceil((FIELD_WIDTH - 2) / 2),
-    y: 0,
-    shape: figure,
-    color: randomColor,
-  };
-};
-
-let figure = getNewFigure();
 
 let nextFigureElem = getNewFigure();
 const removePrevFigure = () => {
   for (let y = 0; y < playBoard.length; y++) {
     for (let x = 0; x < playBoard[y].length; x++) {
-      if (playBoard[y][x] === 1) {
+      if (playBoard[y][x] !== 0) {
         playBoard[y][x] = 0;
       }
     }
@@ -166,14 +197,19 @@ const rotateFigure = () => {
 const fixedFigure = () => {
   for (let y = 0; y < playBoard.length; y++) {
     for (let x = 0; x < playBoard[y].length; x++) {
-      if (playBoard[y][x] === 1) {
-        playBoard[y][x] = 9;
+      if (playBoard[y][x] !== 0) {
+        const value = figure.shape.map((item) =>
+          item.find((elem) => elem !== 0)
+        );
+        const value2 = value.find((elem) => elem !== undefined);
+        playBoard[y][x] = value2;
       }
     }
   }
 };
 const moveFigureDown = () => {
   figure.y += 1;
+  console.log(playBoard);
   if (hasCollision()) {
     figure.y -= 1;
     fixedFigure();
@@ -182,14 +218,13 @@ const moveFigureDown = () => {
     nextFigureElem = getNewFigure();
   }
 };
+
 const hasCollision = () => {
   for (let y = 0; y < figure.shape.length; y++) {
     for (let x = 0; x < figure.shape[y].length; x++) {
       if (
-        figure.shape[y][x] &&
-        (playBoard[figure.y + y] === undefined ||
-          playBoard[figure.y + y][figure.x + x] === undefined ||
-          playBoard[figure.y + y][figure.x + x] === 9)
+        playBoard[figure.y + y] === undefined ||
+        playBoard[figure.y + y][figure.x + x] === undefined
       ) {
         return true;
       }
@@ -217,7 +252,7 @@ const scoring = (isLineRemoved) => {
   if (isLineRemoved) {
     scoreValue += 10;
   }
-  $score.innerHTML = `Score: ${scoreValue}`;
+  $scoreValue.innerHTML = `Score: ${scoreValue}`;
   $level.innerHTML = `Level: ${levelInnerHtml}`;
   if (isLineRemoved && gameSpeed != 100) {
     gameSpeed -= 20;
@@ -266,7 +301,7 @@ const startGame = () => {
   setTimeout(startGame, gameSpeed);
 };
 
-$initialGameSpeed.addEventListener("change", (event) => {
+$initialGameSpeedInput.addEventListener("change", (event) => {
   gameSpeed = event.target.value;
 });
 
